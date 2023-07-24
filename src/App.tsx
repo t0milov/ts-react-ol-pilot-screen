34741d634b5f95ae32b2178f0c7d9dac4813dd2d
t0milov <tomilov.vov-tomilov@yandex.ru>
2023-07-24 23:20:46 +0500

    ol app

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import React, { useContext, useEffect, useRef } from 'react';
+import './App.css';
+import 'ol/ol.css';
+import Map from 'ol/Map.js';
+import OSM from 'ol/source/OSM.js';
+import TileLayer from 'ol/layer/Tile.js';
+import View from 'ol/View.js';
+
+const App: React.FC = () => {
+
+	const olCanvasRef = useRef<any>(null);
+
+	useEffect(() => {
+		if (olCanvasRef.current === null){
+			try{
+				olCanvasRef.current = new Map({
+					target: 'map',
+					layers: [
+						new TileLayer({
+							source: new OSM(),
+						}),
+					],
+					view: new View({
+						center: [0, 0],
+						zoom: 2,
+					}),
+				});
+			}catch(e){
+				console.log(e);
+			}
+		}
+	}, []);
+
+	return (
+		<div style={{
+			width: '100vw', 
+			height: '100vh', 
+		}} id="map"/>
+	);
+};
+
+export default App;
